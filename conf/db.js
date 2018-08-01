f5de00c3f7743dd990074a31cd64dd2512422e1d
jujinjie <jujinjie@163.com>
2018-08-02 01:36:19 +0800

    连接数据库实现

--- conf/db.js
+++ conf/db.js
@@ -5,8 +5,8 @@ module.exports = {
 	mysql: {
 		host: '127.0.0.1', 
 		user: 'root',
-		password: '',
-		database:'test', // 前面建的user表位于这个数据库中
+		password: '550716',
+		database:'group_db', // 前面建的user表位于这个数据库中
 		port: 3306
 	}
 };
--- dao/userSqlMapping.js
+++ dao/userSqlMapping.js
@@ -1,11 +1,11 @@
 // dao/userSqlMapping.js
 // CRUD SQL语句
 var user={
-	insert:'INSERT INTO user(userid,username,phonenum,addr) VALUES(0,?,?,?)',
-	update:'update user set username=? ,phonenum=?,addr=? where userid=?',
-	delete:'delete from user where userid=?',
-	queryById:'select * from user where userid=?',
-	queryAll:'select * from user'
+	insert:'INSERT INTO users(userid,username,phonenum,addr) VALUES(0,?,?,?)',
+	update:'update users set username=? ,phonenum=?,addr=? where userid=?',
+	delete:'delete from users where userid=?',
+	queryById:'select * from users where userid=?',
+	queryAll:'select * from users'
 };
 
 module.exports=user;
